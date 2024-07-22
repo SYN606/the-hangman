9b270775ec0f3434d6ef6a09283ab114e68fa827
SYN606 <postbox.501@yandex.com>
2024-07-22 19:10:05 +0530

    added logic of diffiulty level

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
 
+def difficulty_level():
+    while True:
+        difficulty_level = int(input('choose the difficulty level.\n1. Easy\n2. Medium\n3. Hard\n>>>'))
+        if difficulty_level == 1:
+            print('\nYou choose easy mode.\nIn easy mode there will be 3 letters and 5 attempts to guess it.\n')
+            return 3
+            break
+        elif difficulty_level == 2:
+            print('\nYou choose medium mode.\nIn medium mode there will be 5 letters and 7 attempts to guess it.\n')
+            return 5
+            break
+        elif difficulty_level == 3:
+            print('\nYou choose hard mode.\nIn hard mode there will be 7 letters and 8 attempts to guess it.\n')
+            return 7
+            break
+        else:
+            print('\nSomething went wrong.\n')
 
 def get_words_from_api():
-    url = 'https://random-word-api.herokuapp.com/word?length=5'
+    difficulty = difficulty_level() 
+    url = f'https://random-word-api.herokuapp.com/word?length={difficulty}'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -11,11 +29,11 @@ def get_words_from_api():
         print(f"Failed to retrieve data: {response.status_code}")
 
 word = get_words_from_api()
-
+print(word)
 
 def hangman(guess_word):
     word_length = len(guess_word)
-    total_attempts = word_length
+    total_attempts = word_length + 2
 
     while total_attempts >= 1:
         user_guess = input("Enter your guess : ")
@@ -28,4 +46,4 @@ def hangman(guess_word):
                 total_attempts -= 1
 
 
-hangman(word)
+# hangman(word)
