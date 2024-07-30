c1dd6e543c69091c5c8ea37d1a55875192951e9f
SYN606 <postbox.501@yandex.com>
2024-07-30 20:23:34 +0530

    Added final hangman Logic.

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import requests
 
+print("Welcome to Hangman!")
+
+
 def difficulty_level():
     while True:
-        difficulty_level = int(input('choose the difficulty level.\n1. Easy\n2. Medium\n3. Hard\n>>>'))
+        difficulty_level = int(
+            input("Choose the difficulty level.\n1. Easy\n2. Medium\n3. Hard\n>>>")
+        )
         if difficulty_level == 1:
-            print('\nYou choose easy mode.\nIn easy mode there will be 3 letters and 5 attempts to guess it.\n')
+            print(
+                "\nYou choose easy mode.\nIn easy mode there will be 3 letters and 5 attempts to guess it.\n"
+            )
             return 3
-            break
+
         elif difficulty_level == 2:
-            print('\nYou choose medium mode.\nIn medium mode there will be 5 letters and 7 attempts to guess it.\n')
+            print(
+                "\nYou choose medium mode.\nIn medium mode there will be 5 letters and 7 attempts to guess it.\n"
+            )
             return 5
-            break
+
         elif difficulty_level == 3:
-            print('\nYou choose hard mode.\nIn hard mode there will be 7 letters and 8 attempts to guess it.\n')
+            print(
+                "\nYou choose hard mode.\nIn hard mode there will be 7 letters and 8 attempts to guess it.\n"
+            )
             return 7
-            break
+
         else:
-            print('\nSomething went wrong.\n')
+            print("\nSomething went wrong.\n")
+
+
+difficulty = difficulty_level()
+
 
 def get_words_from_api():
-    difficulty = difficulty_level() 
-    url = f'https://random-word-api.herokuapp.com/word?length={difficulty}'
+    url = f"https://random-word-api.herokuapp.com/word?length={difficulty}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -28,22 +42,48 @@ def get_words_from_api():
     else:
         print(f"Failed to retrieve data: {response.status_code}")
 
-word = get_words_from_api()
-print(word)
 
-def hangman(guess_word):
-    word_length = len(guess_word)
-    total_attempts = word_length + 2
-
-    while total_attempts >= 1:
-        user_guess = input("Enter your guess : ")
-
-        for j in guess_word:
-            if user_guess == j:
-                print('right guess')
-            else:
-                print('wrong guess')
-                total_attempts -= 1
+word = str(get_words_from_api())
 
 
-# hangman(word)
+def display_word(word, guessed_letters):
+    return " ".join(letter if letter in guessed_letters else "_" for letter in word)
+
+
+def hangman(word, difficulty_level):
+    guessed_letters = set()
+    attempts = difficulty_level + 2  # Number of allowed incorrect guesses
+    guessed_word = display_word(word, guessed_letters)
+
+    print("Try to guess the word.")
+    print(guessed_word)
+
+    while attempts > 0 and "_" in guessed_word:
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You've already guessed that letter.")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in word:
+            print("Good guess!")
+        else:
+            attempts -= 1
+            print(f"Incorrect! You have {attempts} attempts left.")
+
+        guessed_word = display_word(word, guessed_letters)
+        print(guessed_word)
+
+    if "_" not in guessed_word:
+        print("Congratulations! You've guessed the word.")
+    else:
+        print(f"Game over! The word was '{word}'.")
+
+
+hangman(word, difficulty)
